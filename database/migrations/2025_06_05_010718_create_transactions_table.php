db52c33c339b3b81e250b4a661c94f5038089a18
yassir <myassiralhakim@gmail.com>
2025-06-05 08:07:27 +0700

    update

--- database/migrations/2025_06_05_010718_create_transactions_table.php
+++ database/migrations/2025_06_05_010718_create_transactions_table.php
@@ -11,13 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->id();
-            $table->string("nm_brg");
-            $table->integer("hrg_brg");
-            $table->integer("stok");
-            $table->enum("kategori", ["Makanan","Minuman", "Perawatan"]);
-            $table->string("gambar");
             $table->timestamps();
         });
     }
@@ -27,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('transactions');
     }
 };
